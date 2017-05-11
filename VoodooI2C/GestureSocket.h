bca88f938e81daa3ebe20cf4ffd9e76f57537bfd
Kishor Prins <me@68x.ca>
2017-05-10 22:36:00 -0400

    Add in message support

--- VoodooI2C/GestureSocket.h
+++ VoodooI2C/GestureSocket.h
@@ -12,6 +12,8 @@
 #include <sys/kern_control.h>
 #include <sys/sysctl.h>
 
+#include "csgesture-softc.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -25,6 +27,7 @@ extern "C" {
 
 kern_return_t initialise_gesture_socket();
 kern_return_t destroy_gesture_socket();
+void send_input(struct csgesture_softc* sc);
     
 #ifdef __cplusplus
 }
