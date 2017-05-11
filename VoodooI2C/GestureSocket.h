2e557d4019f1c9e7602a9bc02a8fea8836a28a59
Kishor Prins <me@68x.ca>
2017-05-10 21:34:52 -0400

    Add the base socket for kernel<->user comms

---
+++ VoodooI2C/GestureSocket.h
@@ -0,0 +1,33 @@
+//
+//  GestureSocket.h
+//  VoodooI2C
+//
+//  Copyright © 2017 Kishor Prins. All rights reserved.
+//
+
+#ifndef GestureClient_h
+#define GestureClient_h
+
+#include <libkern/libkern.h>
+#include <sys/kern_control.h>
+#include <sys/sysctl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ This header and implementation files will allow a user mode client 
+ to communicate with the VoodooI2C driver.
+ */
+
+#define GESTURE_CLIENT_CTL_NAME "com.alexandred.VoodooI2C.GestureSocket"
+
+kern_return_t initialise_gesture_socket();
+kern_return_t destroy_gesture_socket();
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GestureClient_h */
