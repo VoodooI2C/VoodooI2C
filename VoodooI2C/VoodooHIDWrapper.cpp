5af022e55ea84e86a4bf791b9a244b61adef8876
CoolStar <coolstarorganization@gmail.com>
2017-08-14 12:07:23 -0700

    Update VoodooHIDWrapper to compile with rewritten VoodooI2CHIDDevice. Remove logs.

--- VoodooI2C/VoodooHIDWrapper.cpp
+++ VoodooI2C/VoodooHIDWrapper.cpp
@@ -24,34 +24,28 @@ bool VoodooHIDWrapper::start(IOService *provider) {
     if (OSDynamicCast(VoodooI2CHIDDevice, provider) == NULL)
         return false;
 
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    setProperty("HIDDefaultBehavior", OSString::withCString("Trackpad"));
+    setProperty("HIDDefaultBehavior", OSString::withCString("Mouse"));
     return IOHIDDevice::start(provider);
 }
 
 IOReturn VoodooHIDWrapper::setProperties(OSObject *properties) {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
     return kIOReturnUnsupported;
 }
 
 IOReturn VoodooHIDWrapper::newReportDescriptor(IOMemoryDescriptor **descriptor) const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    IOBufferMemoryDescriptor *buffer = IOBufferMemoryDescriptor::inTaskWithOptions(kernel_task, 0, GetOwner(this)->ihid->hdesc.wReportDescLength);
+    IOBufferMemoryDescriptor *buffer = IOBufferMemoryDescriptor::inTaskWithOptions(kernel_task, 0, GetOwner(this)->hid_device->hdesc.wReportDescLength);
 
     if (buffer == NULL) return kIOReturnNoResources;
     GetOwner(this)->write_report_descriptor_to_buffer(buffer);
     *descriptor = buffer;
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
     return kIOReturnSuccess;
 }
 
 IOReturn VoodooHIDWrapper::setReport(IOMemoryDescriptor *report, IOHIDReportType reportType, IOOptionBits options) {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
     return kIOReturnUnsupported;
 }
 
 IOReturn VoodooHIDWrapper::getReport(IOMemoryDescriptor *report, IOHIDReportType reportType, IOOptionBits options) {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
     return kIOReturnUnsupported;
 }
 
@@ -63,46 +57,37 @@ IOReturn VoodooHIDWrapper::handleReport(
 }
 
 OSString* VoodooHIDWrapper::newManufacturerString() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    return OSString::withCString("Lynx Point");
+    return OSString::withCString("Apple");
 }
 
 OSNumber* VoodooHIDWrapper::newPrimaryUsageNumber() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    return OSNumber::withNumber(kHIDUsage_Dig_TouchScreen, 32);
+    return OSNumber::withNumber(kHIDUsage_GD_Mouse, 32);
 }
 
 OSNumber* VoodooHIDWrapper::newPrimaryUsagePageNumber() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    return OSNumber::withNumber(kHIDPage_Digitizer, 32);
+    return OSNumber::withNumber(kHIDPage_GenericDesktop, 32);
 }
 
 OSNumber* VoodooHIDWrapper::newProductIDNumber() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    return OSNumber::withNumber(GetOwner(this)->ihid->hdesc.wProductID, 16);
+    return OSNumber::withNumber(GetOwner(this)->hid_device->hdesc.wProductID, 16);
 }
 
 OSString* VoodooHIDWrapper::newProductString() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    return OSString::withCString("Synaptics 7500 Clearpad");
+    return OSString::withCString("I2C HID Device");
 }
 
 OSString* VoodooHIDWrapper::newSerialNumberString() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
     return OSString::withCString("1234");
 }
 
 OSString* VoodooHIDWrapper::newTransportString() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
     return OSString::withCString("I2C");
 }
 
 OSNumber* VoodooHIDWrapper::newVendorIDNumber() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
-    return OSNumber::withNumber(GetOwner(this)->ihid->hdesc.wVendorID, 16);
+    return OSNumber::withNumber(GetOwner(this)->hid_device->hdesc.wVendorID, 16);
 }
 
 OSNumber* VoodooHIDWrapper::newLocationIDNumber() const {
-    IOLog("VoodooI2C: %s, line %d\n", __FILE__, __LINE__);
     return OSNumber::withNumber(123, 32);
 }
