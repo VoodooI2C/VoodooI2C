4e4b3c032b3474c3c6512fcb061e5a643a29b2ca
CoolStar <coolstarorganization@gmail.com>
2017-08-23 15:10:54 -0700

    Add support for APIC and GPIO interrupt handling on the device nub

--- VoodooI2C/VoodooI2C/VoodooI2CDevice/VoodooI2CDeviceNub.hpp
+++ VoodooI2C/VoodooI2C/VoodooI2CDevice/VoodooI2CDeviceNub.hpp
@@ -13,6 +13,7 @@
 #include <IOKit/IOKitKeys.h>
 #include <IOKit/IOService.h>
 #include <IOKit/acpi/IOACPIPlatformDevice.h>
+#include "VoodooGPIO.hpp"
 
 class VoodooI2CDeviceNub : public IOService {
   OSDeclareDefaultStructors(VoodooI2CDeviceNub);
@@ -27,11 +28,24 @@ class VoodooI2CDeviceNub : public IOService {
     void free();
     bool start(IOService* provider);
     void stop(IOService* provider);
-
+    
+    virtual IOReturn disableInterrupt(int source) override;
+    virtual IOReturn enableInterrupt(int source) override;
+    virtual IOReturn getInterruptType(int source, int *interruptType) override;
+    virtual IOReturn registerInterrupt(int source, OSObject *target, IOInterruptAction handler, void *refcon) override;
+    virtual IOReturn unregisterInterrupt(int source);
  protected:
  private:
     IOACPIPlatformDevice *acpi_device;
     bool get_device_resources();
+    
+    VoodooGPIO *gpioController;
+    VoodooGPIO *getGPIOController();
+    
+    bool hasGPIOInt;
+    
+    UInt16 gpioPin;
+    int gpioIRQ;
 };
 
 
