b9ccbf4fbf0c88078c0710c0b76bc2dc7a7168db
Kishor Prins <me@68x.ca>
2017-10-15 11:28:46 -0400

    Allocate member for static members

--- Dependencies/VoodooI2CServices/VoodooI2CServices/VoodooI2CServices.cpp
+++ Dependencies/VoodooI2CServices/VoodooI2CServices/VoodooI2CServices.cpp
@@ -8,6 +8,8 @@
 
 #include "VoodooI2CServices.hpp"
 
+const IORegistryPlane* VoodooI2CServices::gVoodooI2CPlane = NULL;
+
 #define super IOService
 OSDefineMetaClassAndStructors(VoodooI2CServices, IOService);
 
