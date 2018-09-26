518f6cc2859146445a5d2e1a5cee45c6bd6d830b
Alexandre <alex.daoud@mac.com>
2018-09-26 02:41:55 +0100

    update VoodooI2CHID

--- VoodooI2C/VoodooI2C/VoodooI2CDevice/VoodooI2CDeviceNub.cpp
+++ VoodooI2C/VoodooI2C/VoodooI2CDevice/VoodooI2CDeviceNub.cpp
@@ -141,20 +141,20 @@ exit:
 
 VoodooGPIO* VoodooI2CDeviceNub::getGPIOController() {
     VoodooGPIO* gpio_controller = NULL;
-    
+
     // Wait for GPIO controller, up to 1 second
     OSDictionary* name_match = OSDictionary::withCapacity(1);
     name_match = IOService::serviceMatching("VoodooGPIO");
-    
+
     IOService* matched = waitForMatchingService(name_match, 1000000000);
     gpio_controller = OSDynamicCast(VoodooGPIO, matched);
-    
+
     if (gpio_controller != NULL) {
         IOLog("%s::Got GPIO Controller! %s\n", getName(), gpio_controller->getName());
     }
     name_match->release();
     OSSafeReleaseNULL(matched);
-    
+
     return gpio_controller;
 }
 
