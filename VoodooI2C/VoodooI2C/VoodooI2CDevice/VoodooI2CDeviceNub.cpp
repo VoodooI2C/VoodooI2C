b08fed2a34563dca28678f872a55a953c1da9d20
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2020-10-11 11:02:27 -0700

    Update VoodooI2CDeviceNub.cpp

--- VoodooI2C/VoodooI2C/VoodooI2CDevice/VoodooI2CDeviceNub.cpp
+++ VoodooI2C/VoodooI2C/VoodooI2CDevice/VoodooI2CDeviceNub.cpp
@@ -89,10 +89,11 @@ IOReturn VoodooI2CDeviceNub::evaluateDSM(const char *uuid, UInt32 index, OSObjec
     };
 
     ret = acpi_device->evaluateObject("XDSM", result, params, 4);
-    if (ret != kIOReturnSuccess)
+    if (ret != kIOReturnSuccess) {
         ret = acpi_device->evaluateObject("_DSM", result, params, 4);
-    else
-        IOLog("%s::%s Warning: support for XDSM method is deprecated and will be removed in future release, please remove related rename.\n", controller_name, getName());
+    } else {
+        IOLog("%s::%s Warning: support for XDSM method is deprecated and will be removed in a future release, please remove related rename.\n", controller_name, getName());
+    }
 
     params[0]->release();
     params[1]->release();
